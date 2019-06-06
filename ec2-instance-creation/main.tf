60ea3a7ba81df3b9c85a8970edf8ffc2bf9fb77c
Himadri Ganguly <himadri@colourdrift.com>
2019-06-07 05:12:33 +0530

    Added ec2-instance-creation terraform project.

---
+++ ec2-instance-creation/main.tf
@@ -0,0 +1,46 @@
+/* 
+Provider defines the Platform Terraform will interact with.
+In this example aws is the provider platform where Terraform
+will create the resources.
+*/
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region = var.region
+  version = "~> 2.8"
+}
+
+# Creates EC2 instance
+resource "aws_instance" "web" {
+  # The OS image that will be used while creating the EC2 instance
+  ami           = var.amis[var.region]
+  
+  # The type of instance selection for the EC2 instance
+  instance_type = "t2.micro" 
+  
+  # Attach the "deployer-key" Key Pair with this instance
+  key_name      = "deployer-key"
+  
+  tags = {
+    Name = "WebServer"
+  }
+}
+
+# Creates AWS Key Pair
+resource "aws_key_pair" "deployer" {
+  key_name    = "deployer-key"
+  public_key  = var.public_key
+}
+
+output "public-ip" {
+  value = aws_instance.web.public_ip
+}
+
+output "private-ip" {
+  value = aws_instance.web.private_ip
+}
+
+output "key-name" {
+  value = aws_instance.web.key_name
+}
+
---
+++ ec2-instance-creation/variables.tf
@@ -0,0 +1,14 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "us-east-1"
+}
+variable "public_key" {}
+
+variable "amis" {
+  type = "map"
+  default = {
+    "us-east-1" = "ami-024a64a6685d05041"
+    "us-west-2" = "ami-005bdb005fb00e791"
+  }
+}
